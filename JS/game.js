1b86ebb093a1a1e7626567219925571fd72d2337
cameronglenn <camglenn8@gmail.com>
2026-03-25 21:33:10 -0400

    Created the 'IsValidInBox()' method that checks the current value and compares it to all values within the given 3x3 box looking for duplicates.

--- JS/game.js
+++ JS/game.js
@@ -60,9 +60,10 @@ export class Soduko
             // List of possible numbers to be added to the cell. 
             let values = [1,2,3,4,5,6,7,8,9]; 
 
-            if (this.IsValidInRow(cell.row, values[i]) === true && this.IsValidInCol(cell.col, values[i]) === true)
+            // See if the current value is valid in the row, column, & box. 
+            if (this.IsValidInRow(cell.row, values[i]) === true && this.IsValidInCol(cell.col, values[i]) === true && this.IsValidInBox(cell, values[i]) === true)
             {
-                // Assign the value to the current cells row and col.
+                // Assign the valid value to the current cells row and col.
                 this.#board[cell.row][cell.col] = values[i];
 
                 // Check to see if the board is fully solved. 
@@ -85,10 +86,44 @@ export class Soduko
 
 
 
+    // Name         : IsValidInBox
+    // Description  : This method is used to see if there's duplicate values within the correct 3x3 box on the board. 
+    // Parameters   : cell{row, col}    :   This is the current cells position on the board. 
+    //                int value         :   This is the current value that's being compared. 
+    // Return Values: bool true         :   If there is no duplicates within the 3x3 box. Otherwise false. 
+    IsValidInBox(cell, value)
+    {
+        // There are 9 3x3 boxes on the board.
+        //  - Find a way to use the current cells row & col to jump to the top left cell position within each 3x3 box to start scanning. 
+        let startingRow = Math.floor(cell.row / 3) * 3;     // This finds the top left starting row position within the 3x3 box.  
+        let startingCol = Math.floor(cell.col / 3) * 3;     // This finds the top left starting column position within the 3x3 box. 
+
+        // Check each row (3 total in a 3x3 box). 
+        for (let i = 0; i < 3; i++)
+        {
+            // Compare the value to the current positions value on the board.
+            for (let j = 0; j < 3; j++)
+            {
+                // Compare the value to the value at this boards position. 
+                if (value === this.#board[startingRow + i][startingCol + j])
+                {
+                    // This indicates that the values is already within the 3x3 box and cannot be placed here. 
+                    return false; 
+                }
+            };
+        };
+
+        // Value can be placed within the 3x3 box.  
+        return true; 
+    };
+
+
+
+
 
     // Name         : IsValidInCol
     // Description  : This method is used to determine whether or not a specfic number is in the same column.
-    // Parameters   : cell{col}     :   This is the current cells position on the baord.
+    // Parameters   : int col       :   This is the current column that the cell is positioned at on the board. 
     //              : int value     :   This is the value used to check if valid in the column. 
     // Return Values: bool true     :   If it's able to go in the current column. Otherwise false. 
     IsValidInCol(col, value)
@@ -115,7 +150,7 @@ export class Soduko
 
     // Name         : IsValidInRow
     // Description  : This method is used to determine whether or not a specfic number is in the same row.
-    // Parameters   : cell{col}    :   This is the current cells position on the baord.
+    // Parameters   : row          :   This is the current row that the cell is positoned at on the board.
     //              : int value    :   This is the value used to check if valid in the row. 
     // Return Values: bool true    :   If it's able to go in the current row. Otherwise false. 
     IsValidInRow(row, value)
@@ -185,16 +220,6 @@ export class Soduko
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
-
-            // [1,2,3,4,5,6,7,8,9],
-            // [1,2,3,4,5,6,7,8,9],
-            // [1,2,3,4,5,6,7,8,9],
-            // [1,2,3,4,0,6,7,8,9],
-            // [1,2,3,4,5,6,7,8,9],
-            // [1,2,3,4,5,6,7,8,9],
-            // [1,2,3,4,5,6,7,8,9],
-            // [1,2,3,4,5,6,7,8,9],
-            // [1,2,3,4,5,6,7,8,9],
         ];
 
         return board; 
